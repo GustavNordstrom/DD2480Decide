4583b7696d239cce056f15ac1bd75b3892bd61d9
WenBiming <m18077021652@163.com>
2025-01-24 20:30:14 +0100

    Add object builder for CMV to make it easier to test on PUM

--- src/main/java/com/dd2480/CMV/CMV.java
+++ src/main/java/com/dd2480/CMV/CMV.java
@@ -1,5 +1,6 @@
 package com.dd2480.CMV;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CMV {
@@ -7,8 +8,32 @@ public class CMV {
     public CMV(List<Boolean> c) {
         this.cmv = c;
     }
+    private CMV(Builder builder) {
+        this.cmv = builder.cmv;
+    }
 
     public List<Boolean> getVector() {
         return cmv;
+    }
+    public static class Builder{
+        private final int size = 15;
+        private List<Boolean> cmv;
+        public Builder() {
+            this.cmv = new ArrayList<>();
+            for (int i = 0; i < size; ++i) {
+                this.cmv.add(Boolean.FALSE);
+            }
+        }
+
+        public Builder setVal(int idx, Boolean val) {
+            this.cmv.set(idx, val);
+            return this;
+
+        }
+
+        public CMV build() {
+            return new CMV(this);
+        }
+
     }
 }
