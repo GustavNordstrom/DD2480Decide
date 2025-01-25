c7422cd3ed8ed7d6b148d1f331af9380ed2c6a0d
WenBiming <m18077021652@163.com>
2025-01-25 01:46:44 +0100

    add condtion class scanner

--- src/main/java/com/dd2480/CMV/impl/ConditionManagerImpl.java
+++ src/main/java/com/dd2480/CMV/impl/ConditionManagerImpl.java
@@ -2,11 +2,16 @@ package com.dd2480.CMV.impl;
 
 import com.dd2480.CMV.Condition;
 import com.dd2480.CMV.ConditionContext;
+import com.dd2480.CMV.ConditionManager;
+import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-public class ConditionManagerImpl {
+public class ConditionManagerImpl implements ConditionManager {
+    private final int conditionNumber = 15;
     private final List<Condition> conditions;
     public List<Boolean> conditionMetVector;
 
@@ -40,4 +45,26 @@ public class ConditionManagerImpl {
     public void addCondition(Condition condition) {
         conditions.add(condition);
     }
+
+    private void retrieveAllConditions() {
+        // Use Reflections to scan for classes implementing Condition in the 'impl' package
+        Reflections reflections = new Reflections("com.dd2480.CMV.impl");  // Replace with your package
+        Set<Class<? extends Condition>> conditionClasses = reflections.getSubTypesOf(Condition.class);
+
+        // Iterate over each class and instantiate it using reflection
+        for (Class<? extends Condition> conditionClass : conditionClasses) {
+            try {
+                // Instantiate the condition class using the default constructor
+                Constructor<? extends Condition> constructor = conditionClass.getConstructor();
+                Condition condition = constructor.newInstance();
+                this.addCondition(condition);
+                // Now you have the condition instance, and you can use it
+                System.out.println("Instantiated condition: " + condition.getClass().getName());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
