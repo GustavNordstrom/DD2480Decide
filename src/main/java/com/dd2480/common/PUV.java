c7baf6ac900d9e339e1163fe982d0b67e9cc81df
WenBiming <m18077021652@163.com>
2025-01-26 22:21:23 +0100

    add PUV object builder to facilitate testing

--- src/main/java/com/dd2480/common/PUV.java
+++ src/main/java/com/dd2480/common/PUV.java
@@ -1,5 +1,7 @@
 package com.dd2480.common;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PUV {
@@ -8,7 +10,29 @@ public class PUV {
         puv = l;
     }
 
+    public PUV(Builder builder) {
+        this.puv = builder.puv;
+    }
     public List<Boolean> getVector() {
         return puv;
+    }
+    public static class Builder {
+        private List<Boolean> puv;
+        public static final int length = 15;
+        public Builder() {
+            this.puv = new ArrayList<Boolean>();
+            for (int i = 0; i < length; ++i) {
+                this.puv.add(Boolean.FALSE);
+            }
+        }
+
+        public Builder setVal(int idx, Boolean val) {
+            this.puv.set(idx, val);
+            return this;
+        }
+
+        public PUV build() {
+            return new PUV(this);
+        }
     }
 }
